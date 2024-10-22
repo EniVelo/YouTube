c479aba6b832dc6fa49213d40100fe19d3b85e87
EniVelo <168634770+EniVelo@users.noreply.github.com>
2024-10-22 22:27:19 +0200

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.search-button').addEventListener('click', function() {
+    const query = document.querySelector('.search-bar').value;
+    alert(`Searching for: ${query}`);
+});
+
